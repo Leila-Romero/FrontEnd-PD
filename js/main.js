0f8225acb677fdd7dfe191cb84e50bffb373b333
Leila Rocio Romero <111443946+Leila-Romero@users.noreply.github.com>
2022-11-09 17:42:42 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function autor() {
+    alert("Autora: Leila Rocio Romero Github: https://github.com/Leila-Romero")
+}
+
+function cambiodefondo() {
+    document.body.style.backgroundColor="#000"
+    document.body.style.color="#eee"
+    document.getElementById("cambiofondoheader").style.backgroundColor="#000"
+    document.getElementById("cambiofondofooter").style.backgroundColor="#3434"
+}
